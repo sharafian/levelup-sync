bb683bf915a7b85eb262e54226d8c424a51379bf
sharafian <ben@sharafian.me>
2019-03-14 17:05:21 -0700

    feat: implement

---
+++ src/index.ts
@@ -0,0 +1,92 @@
+import { LevelUp } from 'levelup'
+
+interface LevelupCacheItem<T> {
+  value: T,
+  freeing?: boolean,
+  writePromise?: Promise<void>,
+  queuedWriteCallback?: () => Promise<void>
+}
+
+export class LevelupSync<T> {
+  private db: LevelUp
+  private cache: {
+    [key: string]: LevelupCacheItem<T>
+  }
+
+  constructor (db: LevelUp) {
+    this.db = db
+    this.cache = {}
+  }
+
+  async load (key: string): Promise<void> {
+    // TODO: handle key not found
+    const value = await this.db.get(key)
+    this.cache[key] = { value }
+  }
+
+  async free (key: string): Promise<void> {
+    const item = this.cache[key]
+
+    if (!item) {
+      return
+    }
+
+    item.freeing = true
+
+    if (item.writePromise) {
+      if (item.queuedWriteCallback) {
+        await item.writePromise
+      }
+      await item.writePromise
+    }
+
+    delete this.cache[key]
+  }
+
+  _getCacheItem (key: string): LevelupCacheItem<T> {
+    const item = this.cache[key]
+
+    if (!item) {
+      throw new Error('item is not in cache. key=' + key)
+    }
+
+    if (item.freeing) {
+      throw new Error('item is being freed. key=' + key)
+    }
+
+    return item
+  }
+
+  getSync (key: string): T {
+    return this._getCacheItem(key).value
+  }
+
+  putSync (key: string, value: T): void {
+    const item = this._getCacheItem(key)
+    item.value = value
+
+    if (!item.writePromise) {
+      item.writePromise = this.db.put(key, value)
+        .then(this._moveQueueCallback.bind(this, item))
+    } else {
+      item.queuedWriteCallback = (() => this.db.put(key, value))
+    }
+  }
+
+  _moveQueueCallback (item: LevelupCacheItem<T>): Promise<void> {
+    if (item.queuedWriteCallback) {
+      item.writePromise = item.queuedWriteCallback()
+        .then(this._moveQueueCallback.bind(this, item))
+
+      delete item.queuedWriteCallback
+      return item.writePromise
+    } else {
+      delete item.writePromise
+      return Promise.resolve()
+    }
+  }
+
+  getLevelUp (): LevelUp {
+    return this.db
+  }
+}
